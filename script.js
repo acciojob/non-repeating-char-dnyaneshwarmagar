5ef3989a1632cda98156996e39f6497d3602167b
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-17 19:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 let charFreq = {};
+
+    // Populate the map with character frequencies
+    for (let char of s) {
+        charFreq[char] = (charFreq[char] || 0) + 1;
+    }
+
+    // Find the first non-repeated character
+    for (let char of s) {
+        if (charFreq[char] === 1) {
+            return char;
+        }
+    }
+
+    // If no non-repeated character found, return null
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
